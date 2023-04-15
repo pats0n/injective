e885fc81fe320556de8e8d98913ffadf4824a7c6
Alex Safonov <pats0nn@gmail.com>
2023-04-15 13:28:07 +0000

    ...

--- query1.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-CHAIN_ID=injective-1
-
-NODE_ADDR=${1:-"https://k8s.global.mainnet.tm.injective.network:443"}
-
-injectived query exchange derivative-markets --chain-id $CHAIN_ID --node=$NODE_ADDR
-
-
